15cc1e8c132230aaada3cb0f318cc903acb0eaa4
Rumesh Sudhaharan <rumesh986@gmail.com>
2021-09-26 01:20:03 +0800

    add support for new server organization and server auth

--- lib/server.dart
+++ lib/server.dart
@@ -18,6 +18,8 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'graphs.dart';
+
 class Plant {
 	final int? id;
 	final String type;
@@ -29,19 +31,36 @@ class Plant {
 	Plant({this.id, required this.type, required this.humidity, required this.temperature, required this.ph, required this.ec});
 }
 
+class System {
+	final String name;
+	final String owner;
+	final String plantType;
+	List<DataPoint> humidity = [];
+	List<DataPoint> temperature = [];
+	List<DataPoint> ph = [];
+	List<DataPoint> ec = [];
+
+	System({required this.name, required this.owner, required this.plantType});
+}
+
 class Server {
 	final _host = 'urban-farming-demo.herokuapp.com';
-	final _port = '80';
-	final userID;
+	// final _port = '3000';
+	String? userID;
 
-	Server(this.userID);
+	Server({this.userID});
 
 	List<Function> _cbs = [];
+	List<Function> _systemCBs = [];
 
-	Uri _buildUri(String path) => Uri.parse('https://$_host/$path');
+	Uri _buildUri(String path) => Uri.parse('http://$_host/$path');
 
 	void addCB(Function cb) {
 		_cbs.add(cb);
+	}
+
+	void addSystemCB(Function cb) {
+		_systemCBs.add(cb);
 	}
 
 	Future<dynamic> _get(String path) async {
@@ -52,10 +71,85 @@ class Server {
 			return jsonDecode(response.body);
 	}
 
+	Future<dynamic> _post(String path, {Map<String, String>? data}) async {
+		Uri uri = _buildUri(path);
+		String jsone = json.encode(data ?? "");
+
+		print("REQ BODY: $jsone");
+		print("REQ BODY LEN: ${jsone.length}");
+
+		var response = await http.post(
+			uri, 
+			body: json.encode(data ?? ""),
+			headers: {
+				"Content-Type": "application/json"
+			},
+		);
+
+		print("STATUS CODE: ${response.statusCode}");
+		print("RESPONSE BODY: ${response.body}");
+
+		if (response.statusCode == 200) {
+			return jsonDecode(response.body);
+		} else {
+			throw Exception("Invalid post");
+		}
+	}
+
+	Future<Map<String, String>> auth(Map<String, String> data) async {
+		try {
+			dynamic respJSON = await _post(
+				"auth/google", 
+				// "users",
+				data: data
+			);
+			print(respJSON);
+			return respJSON;
+		} catch (Exception) {
+			print("Failed to auth");
+			return {};
+		}
+	}
+
 	void _callCB(dynamic retVal) {
 		_cbs.forEach((cb) {
 			cb(retVal);
 		});
+	}
+
+	void _callSystemCB(dynamic retVal) {
+		_systemCBs.forEach((cb) {
+			cb(retVal);
+		});
+	}
+
+	Future<System> getSystem() async {
+		dynamic respJson = await _get("/systems/");
+		System retSystem = System(
+			name: respJson["systemName"],
+			owner: respJson["ownerID"],
+			plantType: respJson["plantType"],
+		);
+
+		respJson["humidity"].forEach((item) {
+			retSystem.humidity.add(DataPoint(item["value"], item["time"]));
+		});
+
+		respJson["temperature"].forEach((item) {
+			retSystem.temperature.add(DataPoint(item["value"], item["time"]));
+		});
+
+		respJson["ph"].forEach((item) {
+			retSystem.ph.add(DataPoint(item["value"], item["time"]));
+		});
+
+		respJson["ec"].forEach((item) {
+			retSystem.ec.add(DataPoint(item["value"], item["time"]));
+		});
+
+		_callSystemCB(retSystem);
+
+		return retSystem;
 	}
 
 	Future<List<Plant>> getPlants() async {
